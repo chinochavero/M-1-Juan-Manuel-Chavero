5e08bcf7ef196fb49b3350bc05a9bb8dba5c2130
Juan Manuel Chavero <chinochavero@hotmail.com>
2023-11-25 12:06:16 -0300

    Primer comentario

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+//test de servidor
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
